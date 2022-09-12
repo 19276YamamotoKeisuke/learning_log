be2873208ee62cbac6fe93bfaf5875d4a4e79e73
yamamoto_k <19276@toba-cmt.ac.jp>
2022-09-12 17:56:18 +0900

    画像フォームは保留
    画像フォームは一時保留→entryの個別ページ作成

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Topic, Entry
+# , UploadImage
 
 admin.site.register(Topic)
 admin.site.register(Entry)
+# admin.site.register(UploadImage)
--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,17 +1,26 @@
 from socket import fromshare
 from django import forms
 
-from .models import Topic, Entry
+from .models import Topic, Entry, UploadImage
+# , UploadImage
 
 class TopicForm(forms.ModelForm):
+    """新規トピック追加用フォーム"""
     class Meta:
         model = Topic
         fields = {'text'}
         labels = {'text': ''}
 
 class EntryForm(forms.ModelForm):
+    """新規記事追加用フォーム"""
     class Meta:
         model = Entry
         fields = Entryfields = ['text']
         labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
+class UploadForm(forms.ModelForm):
+    """新規記事画面の画像追加用フォーム"""
+    class Meta:
+        model = UploadImage
+        fields = ['image']
