0a7260bf1e6bf8182bb304d032330582f2be7799
yamamoto_k <19276@toba-cmt.ac.jp>
2022-12-29 02:07:03 +0900

    profile追加
    profileへ
    姓　名　生年月日をフィールドを追加
    生年月日はプルダウン
    項目はform.pyのMetaの中で指定
    あんまりスマートじゃないけど

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,5 +1,6 @@
 from socket import fromshare
 from django import forms
+from datetime import datetime
 
 from .models import Topic, Entry, Profile
 # , UploadImage
@@ -23,10 +24,12 @@ class EntryForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     """ユーザープロフィール設定フォーム"""
     class Meta:
+        current_year = datetime.now().year
+        min_year = current_year - 100
         model = Profile
-        fields = ['postal_code','prefectures','address','career','introduce']
-        labels = {'postal_code':'郵便番号', 'prefuctures':'都道府県', 'address':'都道府県以降の住所', 'career': '以前所属していた企業・役職', 'introduce':'自己PR'}
-        widgets = {'introduce': forms.Textarea(attrs={'cols': 80})}
+        fields = ['family_name','first_name','sex','birthday','email','postal_code','prefectures','address','career','introduce']
+        labels = {'family_name':'姓','first_name':'名前','sex':'性別','birthday':'生年月日','email':'emailアドレス','postal_code':'郵便番号', 'prefuctures':'都道府県', 'address':'都道府県以降の住所', 'career': '以前所属していた企業・役職', 'introduce':'自己PR'}
+        widgets = {'introduce': forms.Textarea(attrs={'cols': 80}),'birthday':forms.SelectDateWidget(years=[x for x in range(min_year, current_year)])}
 
 # class ApplyForm(forms.ModelForm):
 #     """応募フォームhidden"""
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -39,19 +39,20 @@ class Apply(models.Model):
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner', db_column='owner_id')
     applicant_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', db_column='applicant_id')
 
-    # def __str__(self):
-    #     """admin管理の文字列表現"""
-    #     return self.applicant_id
-
 
 class Profile(models.Model):
     """ユーザープロフィール"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    family_name = models.CharField(verbose_name="苗字", max_length=10)
+    first_name = models.CharField(verbose_name="名前", max_length=10)
     postal_code = models.CharField(verbose_name="郵便番号", max_length=10)
     prefectures = models.CharField(verbose_name="都道府県", choices=settings.PREFECTURES, max_length=4)
     address = models.CharField(max_length=30)
     career = models.CharField(max_length=100) #以前所属していた会社&役職
     introduce = models.CharField(max_length=500) #自己紹介&自己PR文
+    email = models.EmailField() #email専用フィールド @が入ってないとエラー
+    sex = models.CharField(verbose_name="性別", choices=settings.SEX, max_length=5,)
+    birthday = models.DateField(verbose_name="生年月日")
 
     def __str__(self):
         """モデルの文字列表現を返す"""
